ad2cb7e25006cd22604dd62a5945949fecb5263c
slvler <hselviler1@gmail.com>
2024-04-29 09:11:52 +0300

    one click arrival deposit apply (for expired address deposit) (USER_DATA)

---
+++ tests/spot/wallet/DepositCreditApplyTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use Binance\Tests\BaseTestCase;
+use Aeris\GuzzleHttpMock\Expect;
+
+class DepositCreditApplyTest extends BaseTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testDepositCreditApply()
+    {
+        $this->httpMock
+            ->shouldReceiveRequest()
+            ->withUrl(new Expect\Equals('/sapi/v1/capital/deposit/credit-apply'))
+            ->withMethod('POST')
+            ->withQueryParams(new Expect\ArrayEquals([
+                'subUserId' => '500',
+            ]), ['timestamp', 'signature'])
+            ->andRespondWithJson($this->data, $statusCode = 200);
+
+        $response = $this->spotClient->depositCreditApply([
+            'subUserId' => '500'
+        ]);
+
+        $this->assertEquals($response, $this->data);
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+}
